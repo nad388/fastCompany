d65cb6785872e9b2468f53d6be82e5b925304901
nad388 <nad388@yandex.ru>
2022-10-11 21:10:04 +0300

    added simple validate login form

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,17 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '../components/textField';
+import { validator } from '../utils/validator';
 
 const Login = () => {
   const [data, setData] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({});
   const handleChange = ({ target }) => {
     setData((prevState) => ({
       ...prevState,
       [target.name]: target.value,
     }));
   };
+  const validatorConfig = {
+    email: {
+      isRequired: { message: 'Электронная почта обязательна для заполнения' },
+    },
+    password: { isRequired: { message: 'Пароль обязателен для заполнения' } },
+  };
+  useEffect(() => {
+    validate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+  const validate = () => {
+    const errors = validator(data, validatorConfig);
+
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
     console.log(data);
   };
   return (
@@ -21,6 +41,7 @@ const Login = () => {
         name="email"
         value={data.email}
         onChange={handleChange}
+        error={errors.email}
       />
       <TextField
         label="Password"
@@ -28,6 +49,7 @@ const Login = () => {
         name="password"
         value={data.password}
         onChange={handleChange}
+        error={errors.password}
       />
       <button>Submit</button>
     </form>
