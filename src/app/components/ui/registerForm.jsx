2500acea947962377f270fe71f0c4dcc5687231d
nad388 <nad388@yandex.ru>
2022-10-30 22:22:42 +0600

    added radioField

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -3,9 +3,15 @@ import TextField from '../common/form/textField';
 import { validator } from '../../utils/validator';
 import api from '../../api';
 import SelectField from '../common/form/selectField';
+import RadioField from '../common/form/radioField';
 
 const RegisterForm = () => {
-  const [data, setData] = useState({ email: '', password: '', profession: '' });
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    profession: '',
+    sex: 'male',
+  });
   const [professions, setProfessions] = useState();
   const [errors, setErrors] = useState({});
   useEffect(() => {
@@ -85,6 +91,16 @@ const RegisterForm = () => {
         onChange={handleChange}
         value={data.profession}
         error={errors.profession}
+      />
+      <RadioField
+        options={[
+          { name: 'Male', value: 'male' },
+          { name: 'Female', value: 'female' },
+          { name: 'Other', value: 'other' },
+        ]}
+        value={data.sex}
+        name="sex"
+        onChange={handleChange}
       />
 
       <button
